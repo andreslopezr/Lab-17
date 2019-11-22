16b063836376e941a197cdb1a1f160b56f5ef158
Andres Lopez Rosas <andres.lrosas@alumnos.udg.mx>
2019-11-21 22:07:32 -0600

    errores de direccion relativa corregidos

--- Scripts/ensayos a la llama.js
+++ Scripts/ensayos a la llama.js
@@ -52,32 +52,51 @@ function dropCuchara(e)
     var mechero = document.getElementById("mechero");
     var name = "";
     var info = "";
-    for (i=75; i<cuchara.src.length; i++)
-        name += cuchara.src[i];
-    if (name != 'empty.png')
-        mechero.src = "../Resources/Instruments/mechero/mechero " + name;
-    
-    if (name=='cloruro de sodio.png')
+    if(cuchara.src.includes("cloruro"))
+    {
+        name = "cloruro de litio.png";
         info = "Se puede observar un tono rojiso en la llama";
-    else if(name == 'sodio.png')
+    }
+    else if(cuchara.src.includes("sodio"))
+    {
+        name = "sodio.png";
         info = "Se puede notar un amarillo intenso en la llama";
-    else if(name == 'calcio.png')
+    }
+    else if(cuchara.src.includes("calcio"))
+    {
+        name = "calcio.png";
         info = "se puede observar un rojo escarlata en la llama";
-    else if(name == 'potasio.png')
+    }
+    else if(cuchara.src.includes("potasio"))
+    {
+        name = "potasio.png";
         info = "se puede notar un tono lila en la llama";
-    else if(name == 'estroncio.png')
+    }
+    else if(cuchara.src.includes("estroncio"))
+    {
+        name = "estroncio.png";
         info = "se puede notar un tono rojo escarlata en la llama";
-    else if(name == 'bario.png')
+    }
+    else if(cuchara.src.includes("bario"))
+    {
+        name = "bario.png";
         info = "se puede notar un color verde en la llama";
-    else if(name == 'hierro.png')
+    }
+    else if(cuchara.src.includes("hierro"))
+    {
+        name = "hierro.png";
         info = "se puede observar un amarillo brilloso en la llama";
-    else if(name == 'cobre.png')
+    }
+    else if(cuchara.src.includes("cobre"))
+    {
+        name = "cobre.png";
         info = "se puede notar una llama azul";
-    else if(name == 'empty.png')
+    }
+    else if(cuchara.src.includes("empty"))
         info = "";
-    
-    
-        document.getElementById("info").innerHTML = '';
+
+    mechero.src = "../Resources/Instruments/mechero/mechero " + name;
+    document.getElementById("info").innerHTML = '';
     typeWriter(0, info);
 
 }
